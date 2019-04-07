330891d5269cf484ea17f922dce7fde1180fb383
Kwanguk Kim <bangley5@gmail.com>
2019-04-08 01:25:16 +0900

    button layout

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">Calculator</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-content>
+      <Calculator/>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Calculator from "@/components/Calculator.vue";
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+@Component({
+  components: {
+    Calculator,
+  },
+})
+export default class App extends Vue {}
+</script>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="text-xs-center">
+    <v-dialog v-model="dialog" width="400">
+      <template v-slot:activator="{ on }">
+        <v-btn color="red lighten-2" dark v-on="on">Click Me</v-btn>
+      </template>
+
+      <v-card class="grey lighten-3">
+        <v-card-text style="text-align: right;">
+          <div class="subheading">{{displayFormula}}</div>
+          <div class="display-2">{{displayResult}}</div>
+        </v-card-text>
+        <v-card-text class="pa-0">
+          <table style="width:100%; table-layout: fixed; border-spacing: 2px;">
+            <tr>
+              <td>
+                <v-btn block depressed large class="calcbutton" color="grey lighten-2">CE</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton" color="grey lighten-2">C</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton" color="grey lighten-2">BACK</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton" color="grey lighten-2">÷</v-btn>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <v-btn block depressed large class="calcbutton headline">7</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton headline">8</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton headline">9</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton" color="grey lighten-2">×</v-btn>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <v-btn block depressed large class="calcbutton headline">4</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton headline">5</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton headline">6</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton" color="grey lighten-2">-</v-btn>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <v-btn block depressed large class="calcbutton headline">1</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton headline">2</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton headline">3</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton" color="grey lighten-2">+</v-btn>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <v-btn block depressed large class="calcbutton" color="grey lighten-2">±</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton headline">0</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton" color="grey lighten-2">.</v-btn>
+              </td>
+              <td>
+                <v-btn block depressed large class="calcbutton" color="grey lighten-2">=</v-btn>
+              </td>
+            </tr>
+          </table>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" depressed large class="calcbutton" @click="dialog = false">I accept</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class Calculator extends Vue {
+  public dialog: boolean = true;
+  @Prop({ default: "" })
+  public value!: number | string;
+
+  public get displayResult(): string {
+    return "result";
+  }
+
+  public get displayFormula(): string {
+    return "formula";
+  }
+}
+</script>
+
+<style scoped>
+.calcbutton {
+  margin: 0;
+}
+</style>
+
+
+
